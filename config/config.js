a0f094cfe64fad6ac3eed0c2dcb0a173f1e7e858
Alon Gal <alongalky@gmail.com>
2017-04-06 18:36:51 +0300

    Reads env from environment variable

--- config/config.js
+++ config/config.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = (env) => {
+module.exports = (env = process.env.EUREKA_ENV) => {
   let filename = path.join(__dirname, `${env || ''}.config.js`)
   if (!fs.existsSync(filename)) {
+    console.log('No environment specified, using default')
     filename = path.join(__dirname, `default.config.js`)
   }
+
+  console.log('Loading configuration from file', filename)
 
   return require(filename)
 }
