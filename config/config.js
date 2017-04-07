e2fd7f21b1c4bdb3772ec47ce54cdb677878e28a
Andrea Villa <andreakarimodm@gmail.com>
2017-04-07 15:12:15 +0300

    Remove object-merge

--- config/config.js
+++ config/config.js
@@ -1,15 +1,14 @@
 const path = require('path')
-const objectMege = require('object-merge')
 
 module.exports = (fs, readFile, env = process.env.EUREKA_ENV) => {
-  let defaultsfile = path.join(__dirname, 'defaults.config.js')
+  const defaultsfile = path.join(__dirname, 'defaults.config.js')
   let environmentfile = path.join(__dirname, `${env || ''}.config.js`)
   if (!fs.existsSync(environmentfile)) {
     /* We assume local environment as fallback */
     environmentfile = path.join(__dirname, 'local.config.js')
   }
 
-  const config = objectMege(readFile(defaultsfile), readFile(environmentfile))
+  const config = Object.assign(readFile(defaultsfile), readFile(environmentfile))
 
   return config
 }
