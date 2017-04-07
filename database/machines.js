278e2a0cd45c25f367da76c7206779da5c3ff5f0
Alon Gal <alongalky@gmail.com>
2017-04-07 23:28:06 +0300

    Moved account to URL

--- database/machines.js
+++ database/machines.js
@@ -1,14 +1,14 @@
 const database = require('./database')
 
 module.exports = {
-  getMachines: (key) => {
+  getMachines: ({account, key}) => {
     const query =
       'SELECT machines.machine_id, machines.name ' +
       'FROM accounts ' +
       'INNER JOIN machines ON accounts.account_id = machines.account_id ' +
-      'WHERE accounts.key = ? AND accounts.secret = ?'
+      'WHERE accounts.key = ? AND accounts.secret = ? AND accounts.account_id = ?'
 
-    return database().query(query, [key.key, key.secret])
+    return database().query(query, [key.key, key.secret, account])
       .then(([rows, fields]) => rows)
   }
 }
