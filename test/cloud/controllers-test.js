d7da2fca8ef96f0c7d46cb8c1c91db2c412131fe
Andrea Villa <andreakarimodm@gmail.com>
2017-04-21 04:06:36 +0300

    Some tests

--- test/cloud/controllers-test.js
+++ test/cloud/controllers-test.js
@@ -4,20 +4,24 @@ const sinon = require('sinon')
 const sinonStubPromise = require('sinon-stub-promise')
 sinonStubPromise(sinon)
 
-describe('Cloud controller', () => {
+describe.only('Cloud controller', () => {
   const config = {
     cloud_provider: 'google',
     google: {
       region: 'us-east1',
       zone: 'us-east1-b',
       project: 'striped-zebra-11',
-      instance_image: 'awesome-machine'
+      instance_image: 'awesome-machine',
+      docker_registry: 'us.docker.registry.io'
     }
   }
   describe('Agnostic', () => {
     const googleController = {
       controls: 'google',
-      runInstance: sinon.stub()
+      runInstance: sinon.stub(),
+      resolveInstanceExternalIp: sinon.stub(),
+      pushImage: sinon.stub(),
+      pullImage: sinon.stub()
     }
     const database = {
       tasks: {
@@ -25,20 +29,31 @@ describe('Cloud controller', () => {
         changeTaskStatusRunning: sinon.stub(),
         changeTaskStatusError: sinon.stub(),
         changeTaskStatusDone: sinon.stub()
+      },
+      machines: {
+        getMachines: sinon.stub()
       }
     }
+    const dContainer = {
+      commit: sinon.stub()
+    }
+
     const Dockerode = sinon.stub()
-    Dockerode.prototype.pull = sinon.stub()
     Dockerode.prototype.run = sinon.stub()
+    Dockerode.prototype.getContainer = sinon.stub()
+
     beforeEach(() => {
       googleController.runInstance.reset()
+      googleController.resolveInstanceExternalIp.reset()
       database.tasks.changeTaskStatusInitializing.reset()
       database.tasks.changeTaskStatusRunning.reset()
       database.tasks.changeTaskStatusError.reset()
       database.tasks.changeTaskStatusDone.reset()
-      Dockerode.prototype.pull.reset()
+      database.machines.getMachines.reset()
       Dockerode.prototype.run.reset()
+      dContainer.commit.reset()
     })
+
     const controllers = [googleController]
     const persevere = callback => callback()
     const cloud = require('../../cloud/agnostic')({
@@ -48,15 +63,38 @@ describe('Cloud controller', () => {
       controller: controllers.find(c => c.controls === config.cloud_provider),
       persevere
     })
-    it('on happy flow transitions a task to Initializing and then Running', done => {
-      database.tasks.changeTaskStatusInitializing.resolves()
-      googleController.runInstance.resolves({ ip: '1.2.3.4' })
-      Dockerode.prototype.pull.resolves()
-      Dockerode.prototype.run.resolves()
+    const params = {
+      account: '9876',
+      machineName: 'otramachina',
+      command: 'wget the.web'
+    }
+    const taskId = '1234'
+    const remoteImageName = [[config.google.docker_registry, config.google.project, params.account].join('/'), taskId].join(':')
 
-      cloud.runTask('1234', {})
+    it.only('on happy flow transitions a task to Initializing and then Running', done => {
+      database.tasks.changeTaskStatusInitializing.resolves()
+      database.machines.getMachines.resolves([ { name: 'testmachina', vm_id: 'Vm123', container_id: '6969' }, { name: 'otramachina', vm_id: 'Vm987', container_id: 'abcd' } ])
+      googleController.runInstance.resolves({ ip: '1.2.3.4' })
+      googleController.resolveInstanceExternalIp.resolves('9.8.7.6')
+      googleController.pushImage.resolves(remoteImageName)
+      googleController.pullImage.resolves()
+      Dockerode.prototype.run.returns({ on: (event, callback) => callback('1423') })
+      Dockerode.prototype.getContainer.returns(dContainer)
+      database.tasks.changeTaskStatusRunning.resolves()
+      dContainer.commit.resolves()
+
+      cloud.runTask(taskId, params)
         .then(() => {
-          sinon.assert.calledWithMatch(Dockerode, { host: '1.2.3.4' })
+          sinon.assert.calledWithMatch(Dockerode, { host: '9.8.7.6' })
+          sinon.assert.calledWith(database.machines.getMachines, { account: '9876' })
+          sinon.assert.calledWith(Dockerode.prototype.getContainer, 'abcd')
+          sinon.assert.alwaysCalledWith(dContainer.commit, { repo: '9876', tag: '1234' })
+          sinon.assert.calledWithMatch(googleController.pushImage, { taskId: '1234', params } )
+          sinon.assert.calledWith(googleController.resolveInstanceExternalIp, 'Vm987')
+          sinon.assert.calledOnce(googleController.runInstance)
+          sinon.assert.calledWith(googleController.runInstance, '1234', params)
+          sinon.assert.alwaysCalledWithMatch(googleController.pullImage, { image: remoteImageName })
+          sinon.assert.alwaysCalledWithMatch(Dockerode.prototype.run, remoteImageName, params.command.split(' '))
           sinon.assert.calledOnce(database.tasks.changeTaskStatusInitializing)
           sinon.assert.calledOnce(database.tasks.changeTaskStatusRunning)
           sinon.assert.callOrder(database.tasks.changeTaskStatusInitializing, database.tasks.changeTaskStatusRunning)
@@ -181,7 +219,7 @@ describe('Cloud controller', () => {
       .then(vm => {
         sinon.assert.calledOnce(gZone.createVM)
         // Checks if instance gets named correctly
-        sinon.assert.calledWith(gZone.createVM, 'compute-1234', sinon.match.any)
+        sinon.assert.calledWith(gZone.createVM, 'runner-1234', sinon.match.any)
         sinon.assert.calledOnce(gVm.waitFor)
         sinon.assert.calledWith(gVm.waitFor, 'RUNNING')
         if (vm.ip !== '1.2.3.4') {
@@ -199,7 +237,7 @@ describe('Cloud controller', () => {
       googleController.runInstance('1234', {})
       .then(() => {
         sinon.assert.calledOnce(gZone.vm)
-        sinon.assert.calledWith(gZone.vm, 'compute-1234')
+        sinon.assert.calledWith(gZone.vm, 'runner-1234')
         sinon.assert.calledOnce(deleteVm.delete)
         done()
       })
