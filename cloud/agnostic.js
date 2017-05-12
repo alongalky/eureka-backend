da6304f4e9c69bc1f74e29f5ada3631e6b020f60
Andrea Villa <andreakarimodm@gmail.com>
2017-05-12 12:05:07 +0300

    Parallelize VM starting and Machine snapshotting

--- cloud/agnostic.js
+++ cloud/agnostic.js
@@ -24,28 +24,28 @@ module.exports = ({ config, database, Dockerode, controller, persevere }) => {
     terminateTask: taskId =>
       controller.findInstanceForTask(taskId)
         .then(vmId => controller.terminateInstance(vmId)),
-    runTask: (taskId, params) => {
-      return database.tasks.changeTaskStatusInitializing(taskId)
-        .then(() => database.machines.getMachines(params.account))
-        .then(machines => machines.find(machine => machine.name === params.machineName))
-        .then(machine => snapshotMachine({ machine, taskId, params }))
-        .then(imageLocator => database.tiers.getTier(params.tierId)
-          .then(tier =>
-            controller.runInstance({ taskId, tier, params })
-            .then(vm => {
-              const docker = new Dockerode({ host: vm.ip, port: config.docker_port })
-              return persevere(() => controller.pullImage({ docker, image: imageLocator }), Array(6).fill(moment.duration(5, 'seconds')))
-                .then(() => logger.info('Successfully pulled %s on %s', imageLocator, vm.ip))
-                .then(() => {
-                  const wrappedCommand = wrapCommand(params.command, taskId)
-                  return persevereRunImagePromisified({ docker, image: imageLocator, command: wrappedCommand, opts: containerBindsPerAccount(params.account), delays: [moment.duration(5, 'seconds')] })
-                })
-                .then(container => logger.info('Container %s running for task %s', container.id, taskId))
-                .then(() => database.tasks.changeTaskStatusRunning(taskId))
-                .then(() => logger.info('Task %s running', taskId))
+
+    runTask: (taskId, params) =>
+      database.tasks.changeTaskStatusInitializing(taskId)
+        .then(() => Promise.all([
+          database.machines.getMachines(params.account)
+            .then(machines => machines.find(machine => machine.name === params.machineName))
+            .then(machine => snapshotMachine({ machine, taskId, params })),
+          database.tiers.getTier(params.tierId)
+            .then(tier => controller.runInstance({ taskId, tier, params }))
+        ]))
+        .then(([imageLocator, vm]) => {
+          const docker = new Dockerode({ host: vm.ip, port: config.docker_port })
+          return persevere(() => controller.pullImage({ docker, image: imageLocator }), Array(6).fill(moment.duration(5, 'seconds')))
+            .then(() => logger.info('Successfully pulled %s on %s', imageLocator, vm.ip))
+            .then(() => {
+              const wrappedCommand = wrapCommand(params.command, taskId)
+              return persevereRunImagePromisified({ docker, image: imageLocator, command: wrappedCommand, opts: containerBindsPerAccount(params.account), delays: [moment.duration(5, 'seconds')] })
             })
-          )
-        )
+            .then(container => logger.info('Container %s running for task %s', container.id, taskId))
+            .then(() => database.tasks.changeTaskStatusRunning(taskId))
+            .then(() => logger.info('Task %s running', taskId))
+        })
         .catch(err => {
           logger.error('Error starting task', taskId, err)
           return database.tasks.changeTaskStatusError(taskId)
@@ -53,8 +53,8 @@ module.exports = ({ config, database, Dockerode, controller, persevere }) => {
         .catch(err => {
           // TODO: Alert
           logger.error('Task %s deployment failed but status could not be updated', taskId, err)
-        })
-    },
+        }),
+
     resolveInstanceExternalIp: vmId => controller.resolveInstanceExternalIp(vmId),
     getInstanceTags: vmId => controller.getInstanceTags(vmId),
     getBucketForAccount: account => controller.getBucketForAccount(account)
